1b68c6865087f4964c73f2ccd94905f583afa80a
Fitri Nur Azis <fitrinurazis30@gmail.com>
2024-02-25 18:37:18 +0700

    init portfolio

---
+++ src/data/user.js
@@ -0,0 +1,54 @@
+const INFO = {
+	main: {
+		title: "Fitnas | Portfolio",
+		name: "Fitri Nur Azis",
+		email: "fitrinurazis30@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/fitrinurazis",
+		linkedin: "https://linkedin.com/in/fitri-nur-azis/",
+		instagram: "https://instagram.com/azis.nur.1",
+	},
+
+	homepage: {
+		title: "Full-stack web developer.",
+		description:
+			"I am a frontend developer with expertise in ReactJS. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "I’m Fitri Nur A. I live in Indonesia, where I design the future.",
+		description:
+			"I've been working on several projects and I'm proud of the progress I've made. Many of these projects are open source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any fixes or improvements you might think of. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "To Do - List",
+			description:
+				"An o do list is a list of tasks that one must complete. It can be used for daily tasks, weekly tasks, monthly tasks, and even yearly tasks. To do lists are very important because they help us organize our time and ensure that we complete our work on time.",
+			logo: "https://raw.githubusercontent.com/fitrinurazis/To-do-List/main/img/favicon.ico",
+			linkText: "View Project",
+			link: "https://github.com/fitrinurazis/To-do-List",
+		},
+
+		{
+			title: "MizanStore - Clone",
+			description:
+				"Mizanstore.com is a trusted online bookstore that provides the latest, best, and most comprehensive selection of books from all publishers in Indonesia.",
+			logo: "https://mizanstore.com/assets/favicon/favicon-32x32.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
